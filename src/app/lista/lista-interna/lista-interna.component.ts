900581d80e93a55da528244c5374c56282f7cb42
igmelo <igorgalvao@gec.inatel.br>
2020-06-14 19:00:35 -0300

    List of products now show products inside(test)

--- src/app/lista/lista-interna/lista-interna.component.ts
+++ src/app/lista/lista-interna/lista-interna.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ShoppingList, ResponseLista } from 'src/app/models/lista';
 import { ListaService } from 'src/app/services/lista.service';
 import { CookieService } from 'ngx-cookie-service';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-lista-interna',
@@ -13,10 +15,13 @@ export class ListaInternaComponent implements OnInit {
   @Input() list: ShoppingList;
   public isCollapsed = true;
   response: ResponseLista;
-  constructor(private listaService: ListaService, private cookie: CookieService) { }
+  itens: Product;
+  constructor(private listaService: ListaService, private cookie: CookieService, private productService: ProductService) { }
   private userId = this.cookie.get('userId');
   ngOnInit(): void {
+    this.productService.getProducts()
+    .subscribe(res => this.itens = res);
   }
-  
+
 }
 
